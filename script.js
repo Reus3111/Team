e6e001f89f188933489486c28cff679991242c5a
Reus3111 <fabiorossi352@gmail.com>
2023-10-11 13:14:51 +0200

    Controlla bene team-submenu e crea anche gli altri

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 //function that does the animation for the hamburger menu on mobile
 window.onload = function(){
     const menu_button = document.querySelector('.hamburger');
+    const menu=document.querySelector('.mobile-menu');
     menu_button.addEventListener('click',function(){
         menu_button.classList.toggle("is-active");
+        menu.classList.toggle("is-active");
     })
+    const team_button = document.querySelector('.team-button');
+    team_button.addEventListener('click',function(){
+        team_button.classList.toggle("is-active");
+    })
+    /*const projects_button
+    const departments_button*/
 }
 
 //it opens linkedin when clicking the linkedin button
