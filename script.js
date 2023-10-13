d6bcaa9e897b82ade2f7a84c48581f4606a25862
Fabio Rossi <fabiorossi352@gmail.com>
2023-10-13 11:23:00 +0200

    La croce fa ancora schifo

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 window.onload = function(){
     const menu_button = document.querySelector('.hamburger');
     const menu=document.querySelector('.mobile-menu');
+    const team_submenu=document.querySelector('.team-submenu');
     menu_button.addEventListener('click',function(){
         menu_button.classList.toggle("is-active");
         menu.classList.toggle("is-active");
@@ -9,9 +10,11 @@ window.onload = function(){
     const team_button = document.querySelector('.team-button');
     team_button.addEventListener('click',function(){
         team_button.classList.toggle("is-active");
+        team_submenu.classList.toggle("is-active");
     })
-    /*const projects_button
-    const departments_button*/
+    const projects_button = document.querySelector('.projects-button');
+
+    /*const departments_button*/
 }
 
 //it opens linkedin when clicking the linkedin button
